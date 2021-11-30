150ae8966d25bd2c880aaa838fb9c2d2c617e160
Mina Petrovic <mina.petrovic.vp@gmail.com>
2021-11-30 13:12:29 +0100

    Inicijalnizovana LogIn strana

---
+++ index.php
@@ -0,0 +1,84 @@
+<?php
+
+require "dbBroker.php";
+require "models/user.php";
+
+session_start();
+if(isset($_POST['username']) && isset($_POST['password'])){
+    $uname = $_POST['username'];
+    $upass = $_POST['password'];
+
+
+    $user = new User(1, $uname, $upass);
+    $odg = User::logInUser($user, $conn); 
+
+    if($odg->num_rows==1){
+        echo  `
+        <script>
+        console.log( "Uspešno ste se prijavili");
+        </script>
+        `;
+        $_SESSION['user_id'] = $user->id;
+        $_SESSION['active_user']=$user;
+        header('Location: evaluation.php');
+        exit();
+    }else{
+        echo `
+        <script>
+        console.log( "Niste se prijavili!");
+        </script>
+        `;
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
+        integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+  <link rel="stylesheet" href="//cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">
+  <script
+      src="https://code.jquery.com/jquery-3.6.0.js"
+      integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+      crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+          integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+          crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+          integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+          crossorigin="anonymous"></script>
+  <script src="//cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+
+  <title>LogIn</title>
+
+</head>
+<body>
+<body style="background: linear-gradient(90deg, rgba(6,0,116,1) 0%, rgba(9,9,121,1) 20%, rgba(0,212,255,1) 100%);">
+    <div class="login-form">
+        <div class="main-div">
+            <form method="POST" action="#">
+                <div class="container">
+                    <label class="username">Korisnicko ime</label>
+                    <input type="text" name="username" class="form-control"  required>
+                    <br>
+                    <label for="password">Lozinka</label>
+                    <input type="password" name="password" class="form-control" required>
+                    <button type="submit" class="btn btn-primary" name="submit">Prijavi se</button>
+                </div>
+
+            </form>
+        </div>
+
+        
+    </div>
+</body>
+</html>
